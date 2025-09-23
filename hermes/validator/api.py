42cd588a5ce7001cbb804fe97b7774534156e9cc
cool-firer <123917244@qq.com>
2025-09-23 15:27:06 +0800

    feat: add organic signature verify

--- hermes/validator/api.py
+++ hermes/validator/api.py
@@ -1,18 +1,48 @@
-from fastapi import APIRouter, FastAPI, Request
-from fastapi.responses import StreamingResponse
-import bittensor as bt
+from hashlib import sha256
+import time
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, Request
+from substrateinterface import Keypair
 from loguru import logger
-from common.protocol import ChatCompletionRequest, OrganicNonStreamSynapse, OrganicStreamSynapse, SyntheticStreamSynapse
+from common.protocol import ChatCompletionRequest
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from neurons.validator import Validator
 
 
+ALLOWED_SOURCE = ["5FWxwB3DbWvmV9WD2FfojafAw2juiw7MMbc2TQi82SBSgW6Q"]
+
 app = FastAPI()
 router = APIRouter()
 
+async def verify_signature(request: Request):
+    signature = request.headers.get("Hermes-Sign")
+    signed_by = request.headers.get("Hermes-Signed-By")
+    time_stamp = request.headers.get("Hermes-Timestamp")
+
+    if not signature or not signed_by or not time_stamp:
+        raise HTTPException(status_code=400, detail="Missing required signature headers")
+
+    body = await request.body()
+    body_hash = f"{sha256(body).hexdigest()}"
+    logger.info(f"[API] Incoming request body sha256: {body_hash}, signature: {signature}, signed_by: {signed_by}, time_stamp: {time_stamp}")
+
+    if signed_by not in ALLOWED_SOURCE:
+        raise HTTPException(status_code=401, detail="Signer not the expected ss58 address")
+
+    now = int(time.time())
+    if abs(now - int(time_stamp)) > 300:
+        raise HTTPException(status_code=401, detail="Request is too old")
+
+    keypair = Keypair(ss58_address=signed_by)
+    verified = keypair.verify(body_hash, bytes.fromhex(signature))
+
+    if not verified:
+        raise HTTPException(status_code=401, detail="Invalid signature")
+
 @router.post("/{cid}/chat/completions")
-async def chat(cid: str, request: Request, body: ChatCompletionRequest):
+async def chat(
+    cid: str, request: Request, body: ChatCompletionRequest, _: dict = Depends(verify_signature)
+):
     v: "Validator" = request.app.state.validator
     return await v.forward_miner(cid, body)
 
